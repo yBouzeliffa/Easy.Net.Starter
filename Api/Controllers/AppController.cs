8a999318621c0b791233592c6c5f2d19c6165553
bouzeliffay <youssef-bouzeliffa@hotmail.com>
2025-01-06 21:18:30 +0100

    Add App Controller

---
+++ Api/Controllers/AppController.cs
@@ -0,0 +1,20 @@
+﻿using Easy.Net.Starter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Easy.Net.Starter.Api.Controllers
+{
+    public class AppController : BaseController
+    {
+        private readonly AppSettings _appSettings;
+        public AppController(AppSettings appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        [HttpGet("version")]
+        public IActionResult GetVersion()
+        {
+            return Ok(_appSettings.Version);
+        }
+    }
+}
