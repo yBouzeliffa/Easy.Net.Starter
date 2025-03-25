2586aa538927c7875a8efcb52e0bb5d13a851bc3
bouzeliffay <youssef-bouzeliffa@hotmail.com>
2025-03-25 21:05:55 +0100

    Add Result and update packages

---
+++ Models/Result.cs
@@ -0,0 +1,32 @@
+﻿using LanguageExt;
+using System.Net;
+
+namespace Easy.Net.Starter.Models
+{
+    public class Result<T, TError>
+    {
+        private T? _value;
+        private TError? _error;
+
+        public bool IsSuccess { get; }
+
+        public Option<HttpStatusCode> HttpCode = Prelude.Some(HttpStatusCode.OK);
+
+        public T Value
+        {
+            get => IsSuccess ? _value! : throw new InvalidOperationException("Result is not successful");
+            private set => _value = value;
+        }
+
+        public TError Error
+        {
+            get => !IsSuccess ? _error! : throw new InvalidOperationException("Result is successful");
+            private set => _error = value;
+        }
+
+        private Result(bool isSuccess, T? value, TError? error) => (IsSuccess, _value, _error) = (isSuccess, value, error);
+
+        public static Result<T, TError> Success(T value) => new(true, value, default);
+        public static Result<T, TError> Failure(TError error) => new(false, default, error);
+    }
+}
